87e0db434bef76f321f20d5da44aeb117be9c97d
ga-devfront <root@GameComputer.localdomain>
2020-07-01 08:23:19 +0200

    ✨ add Vuetify

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import i18n from '@/plugins/i18n';
 import store from '@/store';
 import router from '@/router';
 import App from './App.vue';
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
@@ -10,5 +11,6 @@ new Vue({
   router,
   store,
   i18n,
+  vuetify,
   render: (h) => h(App),
 }).$mount('#app');
