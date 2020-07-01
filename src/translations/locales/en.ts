80a9c1d64f6691f682562e379c9f361ad9ad3e95
ga-devfront <root@GameComputer.localdomain>
2020-07-01 18:02:17 +0200

    ✨ add required translation for login page

--- src/translations/locales/en.ts
+++ src/translations/locales/en.ts
@@ -1,3 +1,11 @@
 export default {
-  greeting: <string> 'Welcome to Your Vue.js App',
+  login: <object> {
+    connection: <string> 'Connection',
+    username: <string> 'Username',
+    password: <string> 'Password',
+    forgotPass: <string> 'Forgot password ?',
+    new: <string> 'New here ?',
+    createAccount: <string> 'Create an account !',
+    errorMessage: <string> 'The email and password you entered don\'t match.',
+  },
 };
