09fe51e4782ee46f5ff90d5766d36f7b445ce000
ga-devfront <root@GameComputer.localdomain>
2020-07-02 12:29:48 +0200

    ✨ add menu translations

--- src/translations/locales/fr.ts
+++ src/translations/locales/fr.ts
@@ -8,4 +8,17 @@ export default {
     createAccount: <string> 'Créer un compte !',
     errorMessage: <string> 'L\'identifiant et le mot de passe que vous avez saisis ne correspondent pas.',
   },
+  menu: <object> {
+    logout: <string> 'Déconnexion',
+    dashboard: <string> 'Tableau de bord',
+    clients: <string> 'Clients',
+    projects: <string> 'Projets',
+    websites: <string> 'Sites internet',
+    database: <string> 'Bases de données',
+    backups: <string> 'Sauvegardes',
+    billing: <string> 'Facturation',
+    useraccount: <string> 'Comptes utilisateurs',
+    theme: <string> 'Thème sombre/lumineux',
+    lang: <string> 'Changer de langue',
+  },
 };
