ec9af9071e72c5b26bdbb63c134064ee471731cb
ga-devfront <root@GameComputer.localdomain>
2020-07-16 12:06:20 +0200

    ✨ add Dashboard view

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-main>
+    <h1 class="text-center">Dashboard</h1>
+  </v-main>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Dashboard',
+
+  components: {
+  },
+
+  methods: {
+  },
+
+  data: () => ({
+    //
+  }),
+});
+</script>
+
+<style lang="scss" scoped>
+</style>
