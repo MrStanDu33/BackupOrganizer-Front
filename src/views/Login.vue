bf5eef072659d52789d481c135c53ce3f1632d6d
ga-devfront <root@GameComputer.localdomain>
2020-07-16 12:06:10 +0200

    ✨ add API login request

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -14,16 +14,20 @@
         lazy-validation
         align="end">
           <v-text-field
-          :label="$t('login.username')"
+          @keydown.enter="pushLog"
+          :label="$t('login.email')"
+          id="email"
           outlined
           required
           :error="error"
           >
           </v-text-field>
           <v-text-field
+          @keydown.enter="pushLog"
           :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
           :type="showPassword ? 'text' : 'password'"
           name="input-10-2"
+          id="password"
           :label="$t('login.password')"
           @click:append="showPassword = !showPassword"
           outlined
@@ -66,19 +70,32 @@ export default Vue.extend({
   components: {
   },
 
-  computed: {
-  },
-
   methods: {
     pushLog() {
       this.error = false;
       this.errorMessage = '';
       this.loading = true;
       // insert request her
-      window.setTimeout(this.failConnect, 2000);
+      const log = {
+        email: document.getElementById('email').value,
+        password: document.getElementById('password').value,
+      };
+      const xhr = new XMLHttpRequest();
+      xhr.open('POST', `${this.$store.state.API}/user/login`, true);
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.onreadystatechange = () => {
+        if (xhr.status === 200) {
+          this.connected(xhr.response);
+        }
+        if (xhr.status === 401) {
+          this.failConnect();
+        }
+      };
+      xhr.send(JSON.stringify(log));
     },
-    connected() {
-      this.$router.push({ name: 'dashboard' });
+    connected(response) {
+      localStorage.setItem('token', JSON.parse(response).accessToken);
+      this.$router.push({ name: 'Dashboard' });
     },
     failConnect() {
       this.loading = false;
