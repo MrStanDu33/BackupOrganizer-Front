655f7e6df68298df4cb69db59caf9015a119430f
ga-devfront <root@GameComputer.localdomain>
2020-07-17 09:39:56 +0200

    ✨ add new account route

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,5 +1,6 @@
 import Login from '../views/Login.vue';
 import Dashboard from '../views/Dashboard.vue';
+import NewAccount from '../views/NewAccount.vue';
 
 export default [
   {
@@ -10,6 +11,19 @@ export default [
       name: <object> {
         fr: <string> 'Connexion',
         en: <string> 'Connection',
+      },
+      menu: false,
+      auth: false,
+    },
+  },
+  {
+    path: <string> '/newaccount',
+    name: <string> 'New Account',
+    component: NewAccount,
+    meta: <object> {
+      name: <object> {
+        fr: <string> 'Nouveau compte',
+        en: <string> 'New Account',
       },
       menu: false,
       auth: false,
