f0c8390ddedd8c95318c5bd345374181c3e02382
ga-devfront <root@GameComputer.localdomain>
2020-07-17 09:39:45 +0200

    ✨ create new account view

---
+++ src/views/NewAccount.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-main>
+    <h1 class="text-center">New account</h1>
+  </v-main>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'NewAccount',
+
+  components: {
+  },
+
+  methods: {
+  },
+
+  data: () => ({
+    //
+  }),
+});
+</script>
+
+<style lang="scss" scoped>
+</style>
