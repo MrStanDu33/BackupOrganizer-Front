f0bde2763f7308eb4c4701ab8ef72fa24c68c856
ga-devfront <root@GameComputer.localdomain>
2020-07-28 08:15:49 +0200

    ✨ create customer.vue

---
+++ src/views/Customer.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-main>
+  </v-main>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Customer',
+
+  components: {
+  },
+
+  methods: {
+  },
+
+  data: () => ({
+  }),
+});
+</script>
+
+<style lang="scss" scoped>
+</style>
