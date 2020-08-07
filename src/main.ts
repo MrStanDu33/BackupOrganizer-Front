358b48e251e06377f0d948415f4de8c9d25aa72a
ga-devfront <root@GameComputer.localdomain>
2020-08-07 16:02:25 +0200

    📦 update packages

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,8 @@ import Vue from 'vue';
 import i18n from '@/plugins/i18n';
 import store from '@/store';
 import router from '@/router';
-import VueTelInputVuetify from 'vue-tel-input-vuetify/lib';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
-
-Vue.use(VueTelInputVuetify, {
-  vuetify,
-});
 
 Vue.config.productionTip = false;
 
