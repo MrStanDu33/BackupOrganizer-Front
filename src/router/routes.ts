95eb7395d88023165f69f58c26435c425852d528
ga-devfront <root@GameComputer.localdomain>
2020-08-07 16:03:36 +0200

    ✨ add TS and create customer route

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,71 +3,85 @@ import Dashboard from '../views/Dashboard.vue';
 import NewAccount from '../views/NewAccount.vue';
 import Customers from '../views/Customers.vue';
 import CustomerDetails from '../views/CustomerDetails.vue';
+import CreateCustomer from '../views/CreateCustomer.vue';
 
 export default [
   {
-    path: <string> '/login',
-    name: <string> 'Login',
+    path: '/login' as string,
+    name: 'Login' as string,
     component: Login,
     meta: <object> {
       name: <object> {
-        fr: <string> 'Connexion',
-        en: <string> 'Connection',
+        fr: 'Connexion' as string,
+        en: 'Connection' as string,
       },
-      menu: false,
-      auth: false,
+      menu: false as boolean,
+      auth: false as boolean,
     },
   },
   {
-    path: <string> '/newaccount',
-    name: <string> 'New Account',
+    path: '/newaccount' as string,
+    name: 'New Account' as string,
     component: NewAccount,
     meta: <object> {
       name: <object> {
-        fr: <string> 'Nouveau compte',
-        en: <string> 'New Account',
+        fr: 'Nouveau compte' as string,
+        en: 'New Account' as string,
       },
-      menu: false,
-      auth: false,
+      menu: false as boolean,
+      auth: false as boolean,
     },
   },
   {
-    path: <string> '',
-    name: <string> 'Dashboard',
+    path: '' as string,
+    name: 'Dashboard' as string,
     component: Dashboard,
     meta: <object> {
       name: <object> {
-        fr: <string> 'Tableau de bord',
-        en: <string> 'Dashboard',
+        fr: 'Tableau de bord',
+        en: 'Dashboard',
       },
-      menu: true,
-      auth: true,
+      menu: true as boolean,
+      auth: true as boolean,
     },
   },
   {
-    path: <string> '/customers',
-    name: <string> 'Customers',
+    path: '/customers' as string,
+    name: 'Customers' as string,
     component: Customers,
     meta: <object> {
       name: <object> {
-        fr: <string> 'Clients',
-        en: <string> 'Customers',
+        fr: 'Clients' as string,
+        en: 'Customers' as string,
       },
-      menu: true,
-      auth: true,
+      menu: true as boolean,
+      auth: true as boolean,
     },
   },
   {
-    path: <string> '/customer/:customerId',
-    name: <string> 'customerDetails',
+    path: '/customer/:customerId' as string,
+    name: 'customerDetails' as string,
     component: CustomerDetails,
     meta: <object> {
       name: <object> {
-        fr: <string> 'Détails du client',
-        en: <string> 'Customer details',
+        fr: <string> 'Détails du client' as string,
+        en: <string> 'Customer details' as string,
       },
-      menu: true,
-      auth: true,
+      menu: true as boolean,
+      auth: true as boolean,
+    },
+  },
+  {
+    path: '/customer/new' as string,
+    name: 'createCustomer' as string,
+    component: CreateCustomer,
+    meta: <object> {
+      name: <object> {
+        fr: <string> 'Création d\'un nouveau client' as string,
+        en: <string> 'Creation of a new customer' as string,
+      },
+      menu: true as boolean,
+      auth: true as boolean,
     },
   },
 ];
